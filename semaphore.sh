b9193593cfbed82bad2c63ca9cd3570bce0fafa3
Alban Crequy <alban@kinvolk.io>
2016-04-01 14:49:41 +0200

    semaphore.sh: test rkt with the latest godeps

---
+++ semaphore.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = "setup" ] ; then
+  sudo groupadd rkt
+  sudo gpasswd -a runner rkt
+  exit 0
+fi
+
+# Set up go environment on semaphore
+if [ -f /opt/change-go-version.sh ]; then
+    . /opt/change-go-version.sh
+    change-go-version 1.5
+fi
+
+export GOPATH=$HOME/go
+mkdir -p $GOPATH
+mkdir -p $GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
+
+# select rkt sources
+cd
+if [ "$SEMAPHORE_CURRENT_THREAD" = "1" ] ; then
+  BRANCH=master
+elif [ "$SEMAPHORE_CURRENT_THREAD" = "2" ] ; then
+  echo "Build disabled"
+  exit 0
+else
+  echo "SEMAPHORE_CURRENT_THREAD=$SEMAPHORE_CURRENT_THREAD"
+  exit 1
+fi
+
+# install godeps
+go get github.com/tools/godep
+go build github.com/tools/godep
+godep version
+
+# get rkt sources
+go get github.com/coreos/rkt || true
+cd $GOPATH/src/github.com/coreos/rkt
+git checkout $BRANCH
+
+echo "rkt git branch: $BRANCH"
+echo "rkt git describe: $(git describe HEAD)"
+echo "Last two rkt commits:"
+git log -n 2 | cat
+echo
+
+# update godeps
+echo "### getting deps"
+go get github.com/appc/cni || true
+go get github.com/appc/spec || true
+go get go4.org/errorutil || true
+echo "### godep restore"
+godep restore -v
+echo "### updating repositories"
+(cd $GOPATH/src/github.com/appc/cni && git pull origin master)
+(cd $GOPATH/src/github.com/appc/spec && git pull origin master)
+#echo "### godep update appc/cni"
+#godep update -v github.com/appc/cni/...
+#echo "### godep update appc/spec"
+#godep update -v github.com/appc/spec/...
+echo "### godep-save"
+./scripts/godep-save
+echo "### git status"
+git status
+echo "### git diff"
+git diff --no-color
+echo "###"
+
+# updates
+./tests/install-deps.sh
+
+# Test
+./autogen.sh
+./configure --enable-functional-tests \
+      --with-stage1-flavors=coreos,fly \
+      --enable-tpm=no
+
+make -j 4
+make check
